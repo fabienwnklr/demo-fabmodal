9145948e0b9909c5ff2d14ed5dab4a2e2de2ffa4
fabien <fabien.winkler@outlook.fr>
2020-09-02 14:02:22 +0200

    amélioration responsive + design (theme)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,36 +1,57 @@
-document.getElementById('changeTheme').addEventListener('change', function (e) {
+window.addEventListener('load', function () {
+    if (window.localStorage.getItem('darkly')) {
+        document.getElementById('theme_css').href = 'assets/css/darkly.css';
+    }
+})
+
+document.getElementById('changeTheme').addEventListener('click', function (e) {
     e.preventDefault();
     e.stopPropagation();
 
     if (document.getElementById('theme_css').href.search('default') !== -1) {
         document.getElementById('theme_css').href = 'assets/css/darkly.css';
+        window.localStorage.setItem('darkly', 'true');
     } else {
         document.getElementById('theme_css').href = 'assets/css/default.css';
+        window.localStorage.clear();
     }
 });
 
+let defaultContent = `
+    <p>
+        You like this modal ? You can use FabModal function on console using for example :</p>
+    <p>
+        <code class="border rounded shadow-sm p-1">
+        defaultModal.startLoader();
+        </code> 
+        then 
+        <code class="border rounded shadow-sm p-1">
+        defaultModal.resetContent();
+        </code>
+    </p>
+`;
 document.getElementById('default').addEventListener('click', function (e) {
     e.preventDefault();
     e.stopPropagation();
 
     window.defaultModal = new FabModal({
         title: 'Default FabModal',
-        content : '<p>You like this modal ? You can use FabModal function on console using for example :</p> <p><code class="border rounded shadow-sm p-1">defaultModal.startLoader();</code> then <code class="border rounded shadow-sm p-1">defaultModal.resetContent();</code></p>'
+        content: defaultContent
     });
-})
+});
 
-var timeoutContent = `
+let timeoutContent = `
     <p>(15 sec.) Timeout can be pause on hover with option pauseOnHover: <code>true</code> or on calling to function : </p> 
-    <p class="d-flex align-items-center justify-center">
-        <button onclick="timeoutModal.pauseProgress();" class="border rounded shadow-sm p-1">
+    <p class="d-flex align-items-center justify-center flex-column">
+        <button onclick="timeoutModal.pauseProgress();" class="btn btn-secondary">
             FabModal.pauseProgress();
         </button> 
         then 
-        <button onclick="timeoutModal.resumeProgress();" class="border rounded shadow-sm p-1">
+        <button onclick="timeoutModal.resumeProgress();" class="btn btn-secondary">
             FabModal.resumeProgress();
         </button>
         or 
-        <button onclick="timeoutModal.startProgress(6000);" class="border rounded shadow-sm p-1">
+        <button onclick="timeoutModal.startProgress(6000);" class="btn btn-secondary">
             FabModal.startProgress(6000);
         </button>
     </p>
@@ -38,14 +59,14 @@ var timeoutContent = `
 document.getElementById('timeout').addEventListener('click', function (e) {
     e.preventDefault();
     e.stopPropagation();
-    
+
     window.timeoutModal = new FabModal({
         title: 'Timeout close',
         timeout: 15000,
         timeoutProgressBar: true,
-        content : timeoutContent
+        content: timeoutContent
     });
-})
+});
 
 document.getElementById('iframe').addEventListener('click', function (e) {
     e.preventDefault();
@@ -56,4 +77,4 @@ document.getElementById('iframe').addEventListener('click', function (e) {
         isIframe: true,
         iframeURL: 'https://www.youtube.com/embed/HSsqzzuGTPo',
     })
-})
+});
